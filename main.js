68090f115628fce9fc324732c244d3c3dbae2690
Katushkins <103944846+katuha1@users.noreply.github.com>
2023-03-19 20:42:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+/*=== typed js ===*/
+const typed = new Typed('#multiple-text', {
+    strings: [
+        'Графический дизайнер',
+        'Web Разработчик',
+        'Программист'
+    ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    typeDelay: 1000,
+    loop: true
+});
+
+const typed2 = new Typed('#underline', {
+    strings: ['Hello World_'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 10000,
+    loop: true
+});
